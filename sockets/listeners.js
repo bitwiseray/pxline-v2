9f1ec6dabbd35f52e9cdf51a8b026b473421c858
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-04-18 16:18:50 +0000

    Removed base comments

--- sockets/listeners.js
+++ sockets/listeners.js
@@ -1,11 +1,8 @@
-// socketEvents.js
 module.exports = async function(io) {
   io.on('connection', (socket) => {
     console.log('A user connected');
-    // Define your event listeners here
     socket.on('message', message => {
       socket.broadcast.emit('chat-message', message);
     })
-    // Add more event listeners as needed
   });
 };
