cc66dec16d8a765d2c0caf449351b568237cba4a
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-04-19 15:54:54 +0000

    Added room-based unique chatting.

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,19 +1,37 @@
 const socket = io('/');
 
 socket.on('chat-message', message => {
-  appendMessage(message.sender.image, message.sender.display_name, message.message);
+  appendMessage(message.author.image, message.author.displayname, message.content);
 });
 
 let input = document.getElementById('inp');
 document.querySelector('.send').addEventListener('click', (e) => {
   let contents = input.value;
+  let offExport = () => {
+    if (type === 'room') {
+      return {
+      id: room._id,
+      name: room.title,
+      image: room.icon,
+      members: room.members,
+     }
+    } else {
+      return {
+        id: extuser._id,
+        name: extuser.displayname,
+        image: extuser.image,
+        members: null
+      }
+    }
+  }
   socket.emit('message', {
-    message: contents,
-    sender: {
-      display_name: user.display_name,
-      user_name: user.user_name,
+    content: contents,
+    author: {
+      displayname: user.display_name,
+      username: user.user_name,
       image: user.image,
     },
+    chat: offExport(),
     attachments: null,
   });
   appendMessage(user.image, user.display_name, contents);
--- sockets/listeners.js
+++ sockets/listeners.js
@@ -2,7 +2,8 @@ module.exports = async function(io) {
   io.on('connection', (socket) => {
     console.log('A user connected');
     socket.on('message', message => {
-      socket.broadcast.emit('chat-message', message);
+      socket.join(message.chat.id);
+      socket.broadcast.to(message.chat.id).emit('chat-message', message);
     })
   });
 };
