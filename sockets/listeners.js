72d1e7639a167b2d9b157007ae54a0caadda26e5
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-04-19 17:30:04 +0000

    Added handler for user being disconnected & changes for `saveChat` fn

--- sockets/listeners.js
+++ sockets/listeners.js
@@ -1,9 +1,19 @@
+const saveChat = require('../utils/chats-offloader');
+
 module.exports = async function(io) {
+  let chats = [];
+  let chatId;
   io.on('connection', (socket) => {
     console.log('A user connected');
     socket.on('message', message => {
       socket.join(message.chat.id);
       socket.broadcast.to(message.chat.id).emit('chat-message', message);
-    })
+      chats.push(message);
+      chatId = message.chat.chat_id;
+    });
+    socket.on('disconnect', () => {
+      saveChat(chatId, chats);
+      console.log('A user disconnected');
+    });
   });
 };
