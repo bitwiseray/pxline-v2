99a876987fd1e52f1790eb031ff248292c96be3e
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-04-22 07:20:00 +0000

    Added proper try catch

--- utils/chats-offloader.js
+++ utils/chats-offloader.js
@@ -2,20 +2,23 @@ const Chat = require('../schematics/chats');
 
 async function saveChats(id, chats) {
   if (!chats) return;
-  const toInsertArray = [];
-  chats.forEach(chat => {
-    toInsertArray.push({
-      content: chat.content || null,
-      sender: chat.author.id,
-      attachments: chat.attachments || null
-    });
+  try {
+     const toInsertArray = [];
+     chats.forEach(chat => {
+      toInsertArray.push({
+        content: chat.content || null,
+        sender: chat.author.id,
+        attachments: chat.attachments || null
+      });
   });
-
   await Chat.updateOne({ _id: id }, { 
     $push: { 
       svd_chats: { $each: toInsertArray } 
-    } 
+    }
   });
+  } catch (error) {
+     console.error(error);
+  }
 }
 
 module.exports = saveChats;
