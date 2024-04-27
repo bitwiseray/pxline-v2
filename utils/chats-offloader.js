ae7d6160d443f805d7cafe085b3ced81c9a1a8ed
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-04-27 14:00:35 +0000

    Added delete after 24hr system.

--- utils/chats-offloader.js
+++ utils/chats-offloader.js
@@ -3,28 +3,39 @@ const Chat = require('../schematics/chats');
 async function saveChats(id, chats) {
   if (!chats || !chats.length) return;
   try {
-    const chat = await Chat.findById(id);
+    let chat = await Chat.findById(id);
+    if (!chat) return;
     const toInsertArray = [];
-    chats.forEach(newChat => {
-      if (chat.svd_chats.some(existingChat => (
+    const noneMatch = chats.every(newChat => {
+      if (!chat.svd_chats.some(existingChat => (
         existingChat.content.text === newChat.content.text &&
         existingChat.content.timestamp === newChat.content.timestamp &&
         existingChat.sender === newChat.author.id
       ))) {
+        toInsertArray.push({
+          content: { text: newChat.content.text || null, timestamp: newChat.content.timestamp },
+          sender: newChat.author.id,
+          attachments: newChat.attachments || null
+        });
         return true;
       }
-      toInsertArray.push({
-        content: { text: newChat.content.text || null, timestamp: newChat.content.timestamp },
-        sender: newChat.author.id,
-        attachments: newChat.attachments || null
+      return false;
+    });
+    if (noneMatch) {
+      await chat.updateOne({ _id: id }, { 
+        $push: { 
+          svd_chats: { $each: toInsertArray } 
+        }
       });
-    });
-    
-    await chat.updateOne({ _id: id }, { 
-      $push: { 
-        svd_chats: { $each: toInsertArray } 
-      }
-    });
+      const currentDate = Date.now()
+      chat.timestamp = currentDate;
+      chat.forEach(nowChat => {
+        if (currentDate - nowChat.timestamp > 24 * 60 * 60 * 1000) {
+          nowChat.svd_chats = [];
+        }
+      });
+      await chat.save();
+    }
   } catch (error) {
     console.error(error);
   }
