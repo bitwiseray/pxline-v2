2f19636f7f28b9dc809c54b4b1327d2ac2510381
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-04-27 13:20:01 +0000

    Fixed accessing invalid entries & added dynamic path option

--- schematics/media.js
+++ schematics/media.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const media = new mongoose.Schema({
   loadType: { type: String, required: true }, 
-  data: { type: Binary, required: true }, 
+  data: { type: Buffer, required: true }, 
   contentType: { type: String, required: true },
+  createdAt: { type: Number }
 });
 
 const Media = mongoose.model('media', media);
