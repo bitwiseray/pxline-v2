518fc210925317d46fb8471bab7032f5532de1a6
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-04-27 09:20:27 +0000

    Fixed path issue

--- utils/upload-sys.js
+++ utils/upload-sys.js
@@ -1,7 +1,8 @@
 const multer = require('multer');
+const path = require('path');
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '../tmp');
+    cb(null, 'tmp');
   },
   filename: (req, file, cb) => {
     cb(null, `${file.fieldname}-${Date.now()}`);
