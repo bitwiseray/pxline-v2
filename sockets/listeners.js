51b6940eb0565dda7aa9d5c020baeca62e996815
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-05-12 10:11:39 +0000

    Removed unwanted logs

--- sockets/listeners.js
+++ sockets/listeners.js
@@ -17,7 +17,6 @@ module.exports = async (io) => {
     socket.on('message', async message => {
       if (message && message.content && message.author) {
         chats.push(message);
-        console.log('message passed', { text: message.content.text, attachments: message.attachments });
         chatId = message.chat.chat_id;
       }
       io.to(globId).emit('messageAdd', message);
