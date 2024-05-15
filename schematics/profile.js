6140b7f58ed00cab00478b04be85574641ce98c4
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-05-15 15:05:20 +0000

    Made changes out of desperation

--- schematics/profile.js
+++ schematics/profile.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const profile_schema = new mongoose.Schema({
   user_name: { type: String, required: true, unique: true },
   display_name: { type: String },
-  socials: { type: Object },
   password: { type: String, required: true },
   authority: { type: String },
   image: { type: String },
   createdAt: Number,
+  socials: { bio: String, friends: Number, interactions: Number },
   chats: [{ chat_id: String, user_id: String, chat_type: String, total_length: Number }],
 });
 
