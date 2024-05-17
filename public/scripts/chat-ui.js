d7049ae6c05efa2f900bff4715f70a89ba0ade7b
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-05-17 14:49:26 +0000

    Fixed throwup of errors in homescreen or somewhere else

--- public/scripts/chat-ui.js
+++ public/scripts/chat-ui.js
@@ -99,7 +99,7 @@ function formatTimestamp(timestamp, compact) {
 }
 
 const textarea = document.getElementById('inp');
-textarea.addEventListener('input', () => {
+textarea?.addEventListener('input', () => {
   textarea.style.height = '';
   const scrollHeight = textarea.scrollHeight;
   const maxHeight = 90;
