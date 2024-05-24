cd8113c2f85b94606c6ef8a74fb961739e0a6a61
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-05-24 14:37:46 +0000

    Added source REST for chats

--- routes/source-routes.js
+++ routes/source-routes.js
@@ -5,7 +5,7 @@ const UserSources = require('../utils/sourcing/Users');
 const { getIndexes, uploadMedia, getLastMessages, loadFriends } = require('../utils/sourcing');
 const { checkAuth, checkNotAuth } = require('../preval/validators');
 
-router.get('/indexes', async (request, reply) => {
+router.get('/indexes', checkAuth, async (request, reply) => {
   try {
     const offload = await getIndexes(request.user);
     let collectedIds = [];
@@ -28,6 +28,33 @@ router.get('/indexes', async (request, reply) => {
       friends: friends
     }
     reply.status(200).json(toSendData);
+  } catch (e) {
+    request.flash('error', 'Something went wrong');
+    reply.status(500).send({ message: 'Something went wrong' });
+    console.error({ at: '/', error: e });
+  }
+});
+
+
+router.get('/chat/:id/', checkAuth, async (request, reply) => {
+  try {
+    const id = request.params.id;
+    const type = await id.checkIdType();
+    if (type === 'room') {
+      const offload = await RoomSources.loadRoom(id, '_id display_name user_name image createdAt');
+      const toSendData = { 
+        extType: 'room', 
+        extusers: offload.members, 
+        extroom: offload.room, 
+        chats: offload.chats, 
+        user: request.user 
+      }
+      reply.status(200).json(toSendData);
+    } else {
+      const usrOffload = await UserSources.loadUser(id, request.user._id, '_id user_name display_name image chats createdAt');
+      const toSendData = { extType: 'DM', extusers: usrOffload.user, chats: usrOffload.chats, extroom: null, user: request.user };
+      reply.status(200).json(toSendData);
+    }
   } catch (e) {
     request.flash('error', 'Something went wrong');
     reply.status(500).send({ message: 'Something went wrong' });
