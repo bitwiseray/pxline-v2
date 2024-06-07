4b3ea47e0e445be0f9a37945b7d5dc03c0f19a68
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-06-07 20:08:34 +0000

    Fixed icons not showing for toast messages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -56,7 +56,7 @@ const parent = document.querySelector('.toast-container');
 function popToast(type, str) {
   let toast = document.createElement('div');
   toast.classList.add('toast');
-  toast.innerHTML = type === 'error' ? `<i class="fa-regular fa-circle-xmark error"></i> ${str}` : `<i class="fa-regular fa-circle-check"></i> Success!`;
+  toast.innerHTML = type === 'error' ? `<i class="material-symbols-outlined error">cancel</i> ${str}` : `<i class="material-symbols-outlined">check_circle</i> Success!`;
   parent.appendChild(toast);
   setTimeout(() => {
     toast.remove();
