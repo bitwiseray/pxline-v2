2c75b57c826fb5537074b80d470a6453047ae699
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-06-07 17:59:38 +0000

    Added new route

--- routes/source-routes.js
+++ routes/source-routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const RoomSources = require('../utils/sourcing/Rooms');
 const UserSources = require('../utils/sourcing/Users');
 const { getIndexes, uploadMedia, getLastMessages, loadFriends } = require('../utils/sourcing');
 const { checkAuth, checkNotAuth } = require('../preval/validators');
+const { storage, clearCache } = require('../utils/upload-sys');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/indexes', checkAuth, async (request, reply) => {
   try {
@@ -65,6 +69,21 @@ router.get('/chat/:id/', checkAuth, async (request, reply) => {
     request.flash('error', 'Something went wrong');
     reply.status(500).send({ message: 'Something went wrong' });
     console.error({ at: '/', error: e });
+  }
+});
+
+const upload = multer({ storage: storage });
+router.post('/media', upload.single('upload'), async (request, reply) => {
+  try {
+    const upload = await uploadMedia('attachment', request.file, fs.readFileSync(path.join(__dirname, '../tmp', request.file.filename)), request);
+    const responseData = {
+      url: upload.url || null,
+    };
+    reply.status(200).json(responseData);
+    clearCache();
+  } catch (error) {
+    console.log(error)
+    reply.status(500).send('Error uploading file');
   }
 });
 
