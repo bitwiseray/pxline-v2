43cf5333ed548bb4f89fd2a31b47ea995da8beba
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-06-07 19:25:18 +0000

    Turned it into reuseable function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -25,6 +25,33 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+function timeAgo(timestamp) {
+  const createdAt = moment(timestamp);
+  const now = moment();
+  const diffYears = now.diff(createdAt, 'years');
+  const diffMonths = now.diff(createdAt, 'months');
+  const diffWeeks = Math.floor(now.diff(createdAt, 'weeks'));
+  const diffDays = Math.floor(now.diff(createdAt, 'days'));
+  let timeAgo;
+  switch (true) {
+    case diffYears > 0:
+      timeAgo = `${diffYears}y`;
+      break;
+    case diffMonths > 0:
+      timeAgo = `${diffMonths}m`;
+      break;
+    case diffWeeks > 0:
+      timeAgo = `${diffWeeks}w`;
+      break;
+    case diffDays > 0:
+      timeAgo = `${diffDays}d`;
+      break;
+    default:
+      timeAgo = '0d';
+  }
+  return timeAgo;
+}
+
 const parent = document.querySelector('.toast-container');
 function popToast(type, str) {
   let toast = document.createElement('div');
