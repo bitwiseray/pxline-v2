b7a7f3d96dce0bd928789397a3609b58d912354c
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-06-08 10:19:10 +0000

    removed whitespace

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const io = require('socket.io')(socket, {
 
 require('dotenv').config();
 require('./sockets/listeners')(io);
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public/views'));
 app.use(express.static(path.join(__dirname, 'public')));
