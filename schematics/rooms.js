2916d25852eb1dae55fb4cf821d6d8c8705aab3b
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-06-16 16:59:07 +0530

    Removed redundant stuff

--- schematics/rooms.js
+++ schematics/rooms.js
@@ -7,7 +7,7 @@ const rooms_schema = new mongoose.Schema({
   settings: { type: Object },
   createdAt: { type: Number },
   socials: { bio: String },
-  chats: { chat_id: String, chat_type: String, total_length: Number },
+  chats: { chat_id: String, chat_type: String },
 });
 
 const Room = mongoose.model('rooms', rooms_schema);
